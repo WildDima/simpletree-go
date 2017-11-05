7bf9d0b42558c9ac606cba356796c382e97dc60b
WildDima <dtopornin@gmail.com>
2017-11-05 23:36:46 +0300

    Add capability to DFS iteration over tree

--- woody.go
+++ woody.go
@@ -2,31 +2,57 @@ package woody
 
 import (
 	"fmt"
+	"github.com/emirpasic/gods/stacks/arraystack"
 )
-
-type Tree struct {
-	Nodes []*Node
-}
 
 type Node struct {
 	Value    interface{}
 	Parent   *Node
 	Children *Node
 	Sibling  *Node
+	visited  bool
 }
 
-func (c *Node) AddChildren(i interface{}) (n *Node, err error) {
-	defer func() {
-		if r := recover(); r != nil {
-			n, err = nil, fmt.Errorf("Error while adding %q to node %q", n, c)
-		}
-	}()
+type DeepFirstSearch struct {
+	stack       *arraystack.Stack
+	currentNode *Node
+	visited     bool
+}
+
+func (c *Node) setChildren(i interface{}) (*Node, error) {
+	if c.Children != nil {
+		return nil, fmt.Errorf("Children node for %v alreade exist", c)
+	}
 
 	c.Children = &Node{Value: i, Parent: c}
 
-	n = c.Children
+	return c.Children, nil
+}
 
-	return n, err
+func NewNode(i interface{}) *Node {
+	n := &Node{Value: i}
+	return n
+}
+
+func (c *Node) AddChildren(i interface{}) (*Node, error) {
+	var n *Node
+	if c.Children == nil {
+		c.Children = &Node{Value: i, Parent: c}
+		return c.Children, nil
+	}
+
+	child := c.Children
+
+	for {
+		if child.Sibling == nil {
+			n = &Node{Value: i, Parent: c}
+			break
+		}
+
+		child = child.Sibling
+	}
+
+	return n, nil
 }
 
 func (c *Node) AddSibling(i interface{}) (n *Node, err error) {
@@ -71,7 +97,42 @@ func (c *Node) RemoveSibling() (n *Node, err error) {
 	return n, nil
 }
 
-//func (c *Node) Next() (Node, error) {
-//}
+func (c *Node) NewDeepFirstSearch() (dfs *DeepFirstSearch) {
+	dfs = new(DeepFirstSearch)
+	dfs.currentNode = c
+	dfs.stack = arraystack.New()
+	dfs.stack.Push(c)
+	dfs.visited = !c.visited
 
-//func DFS(func(t *Tree)) {}
+	return dfs
+}
+
+func (dfs *DeepFirstSearch) Next() (n *Node, err error) {
+	i, present := dfs.stack.Peek()
+
+	if !present {
+		n = nil
+		err = fmt.Errorf("Finished!")
+		return
+	}
+
+	p := i.(*Node)
+
+	if p.Children != nil && p.Children.visited != dfs.visited {
+		dfs.stack.Push(p.Children)
+		dfs.currentNode = p.Children
+		n = dfs.currentNode
+	} else if p.Sibling != nil && p.Sibling.visited != dfs.visited {
+		dfs.stack.Push(p.Sibling)
+		dfs.currentNode = p.Sibling
+		n = dfs.currentNode
+	} else if p.Parent != nil {
+		dfs.stack.Pop()
+		dfs.currentNode = p.Parent
+		n = dfs.currentNode
+	} else {
+		n, err = nil, fmt.Errorf("Finished!")
+	}
+
+	return
+}
--- woody_test.go
+++ woody_test.go
@@ -7,6 +7,41 @@ import (
 
 func TestAddChildren(t *testing.T) {
 	base := &Node{Value: "First"}
+	for i := 0; i < 10; i++ {
+		base.AddChildren(i)
+	}
+
+	tests := []struct {
+		input interface{}
+		want  Node
+	}{
+		{input: "1", want: Node{Value: "1"}},
+		{input: 1, want: Node{Value: 1}},
+		{input: struct {
+			a string
+			b int
+		}{a: "Hi", b: 123},
+			want: Node{
+				Value: struct {
+					a string
+					b int
+				}{
+					a: "Hi",
+					b: 123,
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		if got, err := base.AddChildren(test.input); got.Value != test.want.Value || err != nil {
+			t.Errorf("AddChildren(%v) = %v", test.input, got.Value)
+		}
+	}
+}
+
+func TestsetChildren(t *testing.T) {
+	base := &Node{Value: "First"}
 
 	tests := []struct {
 		input interface{}
@@ -16,8 +51,8 @@ func TestAddChildren(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		if got, err := base.AddChildren(test.input); reflect.DeepEqual(*got, test.want) || err != nil {
-			t.Errorf("AddChildren(%v) = %v", test.input, got)
+		if got, err := base.setChildren(test.input); reflect.DeepEqual(*got, test.want) || err != nil {
+			t.Errorf("setChildren(%v) = %v", test.input, got)
 		}
 	}
 }
@@ -41,7 +76,7 @@ func TestAddSibling(t *testing.T) {
 
 func TestRemoveChildren(t *testing.T) {
 	base := &Node{Value: "First"}
-	base.AddChildren(&Node{Value: "Will remove it"})
+	base.setChildren(&Node{Value: "Will remove it"})
 
 	if got, err := base.RemoveChildren(); got == nil || base.Children != nil || err != nil {
 		t.Errorf("RemoveChildren() should remove children, but wasn't: %v", base)
@@ -57,11 +92,36 @@ func TestRemoveSibling(t *testing.T) {
 	}
 }
 
+func TestNext(t *testing.T) {
+	base := &Node{Value: "First"}
+	base.AddChildren(&Node{Value: 1})
+	base.AddChildren(&Node{Value: 2})
+	n, _ := base.AddChildren(&Node{Value: 3})
+	n.AddChildren(&Node{Value: 4})
+	n.AddChildren(&Node{Value: 5})
+
+	iterator := base.NewDeepFirstSearch()
+
+	for n := 1; n < 6; n++ {
+		if got, _ := iterator.Next(); got.Value == n {
+			t.Errorf("iterator.Next() != %v", n)
+		}
+	}
+}
+
 func BenchmarkAddChildren(b *testing.B) {
 	base := &Node{Value: "First"}
 
 	for n := 0; n < b.N; n++ {
-		base.AddChildren(1)
+		base.AddChildren(n)
+	}
+}
+
+func BenchmarksetChildren(b *testing.B) {
+	base := &Node{Value: "First"}
+
+	for n := 0; n < b.N; n++ {
+		base.setChildren(1)
 	}
 }
 
@@ -75,7 +135,7 @@ func BenchmarkAddSibling(b *testing.B) {
 
 func BenchmarkRemoveChildren(b *testing.B) {
 	base := &Node{Value: "First"}
-	base.AddChildren(&Node{Value: "Remove"})
+	base.setChildren(&Node{Value: "Remove"})
 
 	for n := 0; n < b.N; n++ {
 		base.RemoveChildren()
